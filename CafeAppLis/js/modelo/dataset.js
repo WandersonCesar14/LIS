754c3b3ac04b37878fcb092aff410e9889a5fd04
Wanderson Cesar <88402753+WandersonCesar14@users.noreply.github.com>
2022-02-17 02:07:22 -0300

    Update and rename CafeAppLis/js/model/dataset.js to CafeAppLis/js/modelo/dataset.js

--- CafeAppLis/js/modelo/dataset.js
+++ CafeAppLis/js/modelo/dataset.js
@@ -1,10 +1,14 @@
 const dataset = [
+/** O nome requer “data-” na propriedade do conjunto de dados.
+Se o nome da tag for um “data-tagname”, a propriedade do conjunto de dados usará “dataset.tagname”.**/
   {
     id: 1,
     name: 'Hambúrguer',
     image: 'images/hamburguer.jpg',
     description:
-      'Um hambúrguer é um sanduíche que consiste em um ou mais hambúrgueres cozidos de carne moída, geralmente carne bovina, colocados dentro de um pão ou pão fatiado. O hambúrguer pode ser frito, grelhado, defumado ou grelhado',
+'Um hambúrguer é um sanduíche que consiste em um ou mais hambúrgueres cozidos de carne moída, geralmente carne bovina, colocados dentro de um pão ou pão fatiado. O hambúrguer pode ser frito, grelhado, defumado ou grelhado',
+
+/*  entre a linha 6 ate 10 os elementos de evento JavaScript e interconecte-se com a tag HTML, valor com a propriedade do conjunto de dados. todos twm o id que ligar ohtml a JavaScript  */
   },
   {
     id: 2,
@@ -12,6 +16,7 @@ const dataset = [
     image: 'images/sanduiche.jpg',
     description:
       'O sanduíche (do inglês sandwich), também popularmente chamado de sanduba (português brasileiro) ou sandes (português europeu), é um tipo de alimento que consiste em duas fatias de um pão inteiro, entre as quais é colocada carne, queijo ou outro tipo de alimento.Os sanduíches são, habitualmente, consumidos ao lanche ou como uma refeição rápida, durante o almoço ou o jantar.',
+      /**  entre a linhas 15 ate 18 os elementos de evento JavaScript e interconecte-se com a tag HTML, valor com a propriedade do conjunto de dados. todos twm o id que ligar ohtml a JavaScript  **/
   },
   {
     id: 3,
@@ -19,6 +24,7 @@ const dataset = [
     image: 'images/cafe.jpg',
     description:
       'O café é uma bebida produzida a partir dos grãos torrados do fruto do cafeeiro. É servido tradicionalmente quente, mas também pode ser consumido gelado. O café é um estimulante, por possuir cafeína — geralmente 80 a 140 mg para cada 207 ml dependendo do método de preparação.',
+      /*  entre a linhas 23 ate 26 os elementos de evento JavaScript e interconecte-se com a tag HTML, valor com a propriedade do conjunto de dados. todos twm o id que ligar ohtml a JavaScript  */
   },
   {
     id: 4,
@@ -26,7 +32,8 @@ const dataset = [
     image: 'images/suco.jpg',
     description:
       'Um suco (português brasileiro) ou sumo (português europeu) é uma bebida produzida do líquido extraído de frutos. Além dos frutos, sucos também podem ser obtidos pelo processamento de outras partes dos vegetais, tais como folhas, talos ou raízes.',
+      /**  entre a linhas 31 ate 34 os elementos de evento JavaScript e interconecte-se com a tag HTML, valor com a propriedade do conjunto de dados. todos twm o id que ligar ohtml a JavaScript  **/
   },
 ];
-
+/* O padrão default pode-se ter várias exportações explícitas por módulo. */
 export default dataset;
